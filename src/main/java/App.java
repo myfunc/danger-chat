dce80bbf7eaed1c3ef06910a7420d58e19b98756
myfunc <mironov.link@gmail.com>
2019-04-26 18:17:57 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import spark.Spark;
+
+import com.google.common.base.Joiner;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/DataRepository.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class DataRepository {
+    public static void temp() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/dangerchar?" +
+                            "user=root&password=xtremal&useUnicode=true&useJDBCCompliantTimezoneShift=true&" +
+                            "useLegacyDatetimeCode=false&serverTimezone=UTC");
+            stmt = conn.createStatement();
+            if (stmt.execute("SELECT Id, Name FROM tblUser")) {
+                rs = stmt.getResultSet();
+                while(rs.next()){
+                    System.out.printf("%s, %s\n", rs.getString("Id"), rs.getString("Name"));
+                }
+
+            }
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
